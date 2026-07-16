caec88509ad653dade4dd552108a3b8b23b5ead9
official-dhonzy <happyseraphineyaosika@gmail.com>
2026-07-16 18:03:54 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/fireba
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
+  apiKey: "AIzaSyDcY0jmGCWwGlbjtgKvwAGx--YzXdal2wY",
   authDomain: "a-ai-d738d.firebaseapp.com",
   projectId: "a-ai-d738d",
   storageBucket: "a-ai-d738d.firebasestorage.app",
   messagingSenderId: "337673964829",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  appId: "1:337673964829:web:748e0ca91bfa340ea7e5f4",
+  measurementId: "G-ZWPYLZ8RND"
 };
 
 const app = initializeApp(firebaseConfig);
